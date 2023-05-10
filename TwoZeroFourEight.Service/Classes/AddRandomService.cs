984336e4c63f5c9594a34b95417d9b1048fc3b1c
Mihail <vtumihail@gmail.com>
2023-05-10 21:00:56 +0300

    Fixed a moves bug

--- TwoZeroFourEight.Service/Classes/AddRandomService.cs
+++ TwoZeroFourEight.Service/Classes/AddRandomService.cs
@@ -5,6 +5,7 @@ namespace TwoZeroFourEight.Services.Classes
 	public class AddRandomService : IAddRandomService
 	{
 		private readonly Random _random;
+		private int[,] arrayClone;
 		public AddRandomService(Random random)
 		{
 			_random = random;
@@ -15,14 +16,16 @@ namespace TwoZeroFourEight.Services.Classes
 			int result = _random.Next(size * size);
 			if (array.Cast<int>().All(tile => tile == 0))
 			{
-				array[result / size, result % size] = 2;
-			}
-			while (array.Cast<int>().Any(tile => tile == 0))
+				arrayClone = array.Clone() as int[,];
+                array[result / size, result % size] = 2;
+            }
+			while (!array.Cast<int>().SequenceEqual(arrayClone.Cast<int>()) && array.Cast<int>().Any(tile => tile == 0))
 			{
 				result = _random.Next(16);
 				if (array[result / size, result % size] == 0)
 				{
 					array[result / size, result % size] = 2;
+					arrayClone = array.Clone() as int[,];
 					return;
 				}
 			}
